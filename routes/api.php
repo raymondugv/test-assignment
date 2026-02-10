72c707b83ec17328c89bb13c08612173689c4a08
kam <github@lekienhoanh.dev>
2026-02-10 18:23:50 +0700

    refactor API routes

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\UserAPIController;
+use App\Http\Controllers\UserAuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', function () {
-    return '';
-});
-
-Route::post('/register', function () {
-    return '';
+Route::group(['controller' => UserAuthController::class], function () {
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
 });
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::post('/logout', function () {
-        return '';
-    });
-
     Route::resource('users', UserAPIController::class);
+
+    Route::post('/logout', [UserAuthController::class, 'logout']);
 });
