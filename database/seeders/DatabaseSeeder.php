58dde2c9e8f99360d60d2d4de74f15ba22c8906d
kam <github@lekienhoanh.dev>
2026-02-10 15:14:22 +0700

    update User seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
         ]);
+
+        User::factory(10)->create();
 
         $this->call([
             PostSeeder::class,
