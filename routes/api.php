1d4fd13fc811a069824196f34b0ae3c34c5137a2
kam <github@lekienhoanh.dev>
2026-02-10 16:30:09 +0700

    update API controller

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\UserAPIController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/health', function () {
-    return response()->json([
-        'status' => 'ok',
-    ]);
-});
 
 Route::post('/login', function () {
     return '';
@@ -20,4 +15,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/logout', function () {
         return '';
     });
+
+    Route::resource('users', UserAPIController::class);
 });
